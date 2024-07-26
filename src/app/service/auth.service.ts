f1a91ff3a934d9e662f4005d2914bc1624f17a60
DamianGallardo <damiangspr@gmail.com>
2024-07-26 01:07:37 -0600

    env

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 import { Router } from '@angular/router';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../../environment.development';
 
 @Injectable({
   providedIn: 'root'
